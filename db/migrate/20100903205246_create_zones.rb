19a250b3493a41ef7e17adabc3167d0183f7b6a4
Christopher Peplin <peplin@bueda.com>
2010-09-03 16:55:35 -0400

    Add Zone.

---
+++ db/migrate/20100903205246_create_zones.rb
@@ -0,0 +1,19 @@
+class CreateZones < ActiveRecord::Migration
+  def self.up
+    create_table :zones do |t|
+      t.string :name
+      t.integer :x
+      t.integer :y
+      t.references :region
+
+      t.timestamps
+    end
+    
+    add_index :zones, [:x, :y], :unique => true
+  end
+
+  def self.down
+    drop_table :zones
+    remove_index :zones, [:x, :y]
+  end
+end
