6fc4fc37729eefd7ae34adad410142070f11665e
Christopher Peplin <peplin@bueda.com>
2010-09-03 15:11:20 -0400

    Initial commit of new rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threephase::Application.config.secret_token = '852f2facad47770f62a6efa863f2b69c89616cb5f5e9b56825080ee8b42da1aaf57ab543d9be637acefb4552d29688e7cacf80415da48fee7ab31ce6ab4cefd8'
