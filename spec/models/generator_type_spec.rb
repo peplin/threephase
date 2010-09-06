c99ad0a273d2834094056173ce2492da7bc6ec51
Christopher Peplin <peplin@bueda.com>
2010-09-06 00:23:16 -0400

    Add some derived method tests for GeneratorType, starting to figure out RSpec.

--- spec/models/generator_type_spec.rb
+++ spec/models/generator_type_spec.rb
@@ -27,4 +27,60 @@ describe GeneratorType do
 
   it { should validate_presence_of :tax_credit }
   it { should_not allow_value(-1).for(:tax_credit) }
+
+  context "A GeneratorType instance" do
+    setup do
+      @generator_type = GeneratorType.new
+    end
+
+    it "should return its marginal cost" do
+    end
+
+    it "should return its current revenue" do
+    end
+
+    it "should return its availability" do
+    end
+
+    context "with a renewable fuel" do
+      setup do
+        @generator_type.fuel = Fuel.find_by_renewable(true).first
+      end
+
+      it "should know its fuel is renewable" do
+        assert @generator_type.renewable?
+      end
+
+      it "should know it doesn't burn any fuel" do
+        assert_equal @generator_type.fuel_amount?, 0
+      end
+
+      it "should return a marginal cost independent of fuel price" do
+        original_mc = @generator_type.marginal_cost
+        pending "update market price....of what?"
+        assert_equal original_mc, @generator_type.marginal_cost
+      end
+    end
+
+    context "with a nonrenewable fuel" do
+      setup do
+        @generator_type.fuel = Fuel.find_by_renewable(false).first
+      end
+
+      it "should know its fuel is renewable" do
+        assert_not @generator_type.renewable?
+      end
+
+      it "should return the amount of fuel it burns" do
+        assert_equal @generator_type.fuel_amount?,
+            @generator_type.average_capacity * @generator_type.fuel_efficiency
+      end
+
+      it "should return a marginal cost dependent on fuel price" do
+        original_mc = @generator_type.marginal_cost
+        pending "update market price"
+        assert_not_equal original_mc, @generator_type.marginal_cost
+      end
+    end
+  end
 end
