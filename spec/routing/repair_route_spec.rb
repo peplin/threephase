08a97f7bceeb888b9fdf420ebc85e32082e0b762
Christopher Peplin <peplin@bueda.com>
2010-09-10 16:48:28 -0400

    Add repair route specs.

---
+++ spec/routing/repair_route_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "routing to repairs" do
+  it { {:get, "/game/1/repairs"}.should route_to(:action => :index,
+      :game => 1, :controller => "repairs") }
+
+  it { {:get, "/generator/1/repairs"}.should route_to(:action => :index,
+      :generator => 1, :controller => "repairs") }
+
+  it { {:post, "/generator/1/repairs"}.should route_to(:action => :create,
+      :generator => 1, :controller => "repairs") }
+
+  it { {:get, "/generator/1/repair/2"}.should route_to(:action => :show,
+        :generator => 1, :id => 2, :controller => "repairs") }
+
+  it { {:get, "/repair/1"}.should route_to(:action => :show,
+        :id => 1, :controller => "repairs") }
+
+  it "does not expose a list of all repairs" do
+    {:get => "/repairs"}.should_not be_routable
+  end
+
+  it "does not allow editing of a repair" do
+    {:put => "/generator/1/repair/1"}.should_not be_routable
+  end
+
+  it "does not allow deleting of a repair" do
+    {:delete => "/generator/1/repair/1"}.should_not be_routable
+  end
+end
