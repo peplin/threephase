cca59ec013c3e9eef177c9b11940391a2b550f07
Christopher Peplin <peplin@bueda.com>
2010-09-15 16:43:34 -0400

    Add Generator controller spec.

---
+++ app/controllers/generator_controller.rb
@@ -0,0 +1,2 @@
+class GeneratorController < ApplicationController
+end
--- spec/routing/generator_route_spec.rb
+++ spec/routing/generator_route_spec.rb
@@ -4,8 +4,8 @@ describe "routing to generators" do
   it { {:get, "/game/1/generators"}.should route_to(:action => :index,
         :game => 1, :controller => "generators") }
 
-  it { {:get, "/game/1/zone/2/generators"}.should route_to(
-      :action => :index, :game => 1, :zone => 2, :controller => "generators") }
+  it { {:get, "/game/1/zone/2/generators"}.should route_to(:action => :index,
+      :game => 1, :zone => 2, :controller => "generators") }
 
   it { {:get, "/game/1/generators/new"}.should route_to(:action => :new,
       :game => 1, :controller => "generators") }
@@ -19,7 +19,7 @@ describe "routing to generators" do
   it { {:get, "/generator/1"}.should route_to(:action => :show,
       :id => 1, :controller => "generators") }
 
-  it { {:get, "/generator/1/edit"}.should route_to(:action => :show,
+  it { {:get, "/generator/1/edit"}.should route_to(:action => :edit,
       :id => 1, :controller => "generators") }
 
   it { {:put, "/generator/1"}.should route_to(:action => :update,
