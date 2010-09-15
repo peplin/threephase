31f7c1c5978277b47c0bbce8e739dafa19556f12
Christopher Peplin <peplin@bueda.com>
2010-09-15 14:33:30 -0400

    Add bid controller spec.

---
+++ app/controllers/bid_controller.rb
@@ -0,0 +1,2 @@
+class BidController < ApplicationController
+end
--- spec/routing/bid_route_spec.rb
+++ spec/routing/bid_route_spec.rb
@@ -13,8 +13,17 @@ describe "routing to bids" do
   it { {:post, "/generator/1/bids"}.should route_to(:action => :create,
       :generator => 1, :controller => "bids") }
 
-  it "does not show individual bids" do
-    {:get => "/generator/1/bid/1"}.should_not be_routable
+  it { {:get, "/generator/1/bid/1"}.should route_to(:action => :show,
+        :generator => 1, :bid => 1, :controller => "bids") }
+
+  it { {:get, "/game/1/bid/1"}.should route_to(:action => :show,
+        :game => 1, :bid => 1, :controller => "bids") }
+
+  it { {:get, "/bid/1"}.should route_to(:action => :show,
+        :bid => 1, :controller => "bids") }
+
+  it "does not expose a master list of bids" do
+    {:get => "/bids"}.should_not be_routable
   end
 
   it "does not allow bid updating" do
