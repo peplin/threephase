9df9f2386611827cd47e7e2d08dc90ab372d6b42
Christopher Peplin <peplin@bueda.com>
2010-09-15 17:26:03 -0400

    Add Region controller spec.

---
+++ app/controllers/region_controller.rb
@@ -0,0 +1,2 @@
+class RegionController < ApplicationController
+end
--- spec/routing/region_route_spec.rb
+++ spec/routing/region_route_spec.rb
@@ -1,17 +1,16 @@
 require 'spec_helper'
 
 describe "routing to regions" do
+  it { {:get, "/game/1/regions"}.should route_to(:action => :index,
+      :game => 1, :controller => "regions") }
+
   it { {:get, "/game/1/region/2"}.should route_to(:action => :show,
       :game => 1, :region => 2, :controller => "regions") }
 
-  it { {:get, "/game/1/region"}.should route_to(:action => :show,
-      :game => 1, :controller => "regions") }
+  # TODO do we need post? how are players added to a game?
 
   it { {:put, "/game/1/region/2"}.should route_to(:action => :update,
       :game => 1, :region => 2, :controller => "regions") }
-
-  it { {:put, "/game/1/region"}.should route_to(:action => :update,
-      :game => 1, :controller => "regions") }
 
   it "does not expose a list of all regions" do
     {:get => "/regions"}.should_not be_routable
