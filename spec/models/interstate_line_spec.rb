6a11764c02e07619b180b44a57986a61be8385cf
Christopher Peplin <peplin@bueda.com>
2010-09-17 19:15:09 -0400

    Add spec for operating/operating_level on interstate lines.

--- spec/models/interstate_line_spec.rb
+++ spec/models/interstate_line_spec.rb
@@ -4,7 +4,10 @@ describe InterstateLine do
   it { should belong_to :incoming_region }
   it { should belong_to :outgoing_region }
   it { should belong_to :line_type }
+
   it { should validate_presence_of :accepted}
+  it { should validate_presence_of :operating }
+  it { should validate_presence_of :operating_level }
 
   it { should respond_to :friendly_id }
 
