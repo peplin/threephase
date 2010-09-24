4f4fefa0309b51dcc2c350091b4f90e1f37b2143
Christopher Peplin <peplin@bueda.com>
2010-09-24 18:21:38 -0400

    Attach a region to every new factory'd game

--- spec/factories/game_factory.rb
+++ spec/factories/game_factory.rb
@@ -1,5 +1,6 @@
 Factory.define :game do |g|
   g.regulation_type :unregulated
+  g.after_create { |game| attach_region game }
 end
 
 Factory.define :invalid_game, :parent => :game do |g|
@@ -12,4 +13,8 @@ end
 
 Factory.define :started_game, :parent => :game do |g|
   g.started Time.now
+end
+
+def attach_region game
+  game.regions.create Factory(:region).attributes
 end
