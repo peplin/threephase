d515952a7882ce7b27694f7e1c673f3ce3939460
Christopher Peplin <peplin@bueda.com>
2010-09-29 12:02:34 -0400

    Ignore updated files with autotest.

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,11 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+
+Autotest.add_hook :initialize do |at|
+  at.add_exception(%r{^\./\.git})
+  at.add_exception(%r{^\./db})
+  at.add_exception(%r{^\./log})
+  at.add_exception(%r{^\./tmp})
+  at.add_exception(%r{^\./rerun.txt})
+  at.add_exception(%r{^\./Gemfile.lock})
+end
