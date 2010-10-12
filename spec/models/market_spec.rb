9d87e271ecfcdd11f7678290646696b6506cac69
Christopher Peplin <peplin@bueda.com>
2010-10-12 00:27:28 -0400

    Pass market model spec.

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -9,15 +9,16 @@ describe Market do
 
   context "A Market instance" do
     before :all do
-      @market = Factory :market
-      @game = Factory :game
+      @price = Factory :market_price
+      @market = @price.market
+      @game = @price.game
       @state = Factory :state, :game => @game
       @city = Factory :city, :state => @state
     end
 
     it "should have a current price for the overall market" do
       (@market.current_price @game).should eq(
-          @game.market_prices.find_by_market(@market).first.price)
+          @game.market_prices.find_by_market_id(@market).price)
     end
   end
 end
