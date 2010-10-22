4e59ce9d7619ae1f42a7c11da8b0b51e9cf4093f
Christopher Peplin <peplin@bueda.com>
2010-10-22 18:50:54 -0400

    Return actual market price from market, given a game.

--- app/models/market.rb
+++ app/models/market.rb
@@ -6,7 +6,9 @@ class Market < ActiveRecord::Base
   validates :name, :presence => true
 
   def current_price game, city=nil
-    0
+    market_price = market_prices.order(:created_at).find_by_game_id(game)
+    market_price.price if market_price
+    0.0
   end
 
   def to_s
