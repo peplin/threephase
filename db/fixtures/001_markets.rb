522ee427c73fcf2e3eb52f84ee582d76cc9ea8be
Christopher Peplin <peplin@bueda.com>
2010-10-22 15:41:48 -0400

    Add fixtures for generator types, fuel types and markets.

---
+++ db/fixtures/001_markets.rb
@@ -0,0 +1,8 @@
+Market.seed_many([
+  {:id => 1, :name => "Natural Gas"},
+  {:id => 2, :name => "Coal"},
+  {:id => 3, :name => "Gasoline"},
+  {:id => 4, :name => "Low-enriched Uranium"},
+  {:id => 5, :name => "Highly-enriched Uranium"},
+  {:id => 6, :name => "Depleted Uranium"}
+])
