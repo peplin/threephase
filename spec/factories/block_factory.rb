12817099daa55a7d686244597f616ae6c5970329
Christopher Peplin <peplin@bueda.com>
2010-10-29 13:41:56 -0400

    Update block factory with new index attrs.

--- spec/factories/block_factory.rb
+++ spec/factories/block_factory.rb
@@ -20,15 +20,15 @@ end
 
 Factory.define :water_block, :parent => :block do |b|
   b.block_type :water
-  b.water_flow rand(20)
+  b.water_index rand(20)
 end
 
 Factory.define :sunny_block, :parent => :block do |b|
   b.block_type :plains
-  b.sunfall 3000 + rand(4000)
+  b.sun_index 3000 + rand(4000)
 end
 
 Factory.define :windy_block, :parent => :block do |b|
   b.block_type :plains
-  b.wind_speed rand(30)
+  b.wind_index rand(30)
 end
