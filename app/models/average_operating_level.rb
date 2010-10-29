8b48394519632ee08461810d0f5a2721499f530c
Christopher Peplin <peplin@bueda.com>
2010-10-29 18:43:37 -0400

    Abstract normalized average logic to be reusable.

--- app/models/average_operating_level.rb
+++ app/models/average_operating_level.rb
@@ -1,22 +1,22 @@
+require 'simple'
+
 class AverageOperatingLevel < ActiveRecord::Base
+  include SimpleExtensions
+
   belongs_to :technical_component_instance
 
   validates :operating_level, :presence => true, :percentage => true
   validates :technical_component_instance, :presence => true
 
-  def refresh new_operating_level
+  def normalized_operating_level new_operating_level
     old_hours = updated_at.hour + 1
     new_hours = Time.now.utc.hour + 1
     # TODO use game time! game time should return UTC
-    if new_hours == old_hours
-      new_average = operating_level - Float(operating_level) / old_hours
-      new_average += Float(new_operating_level) / old_hours
-    else
-      new_average = Float(operating_level) / new_hours * old_hours
-      new_average += Float(new_operating_level) / new_hours *
-          (new_hours - old_hours)
-    end
-    self.operating_level = new_average
-    save
+    normalized_average operating_level, new_operating_level, old_hours,
+        new_hours
+  end
+
+  def refresh new_operating_level
+    self.operating_level = normalized_operating_level(new_operating_level)
   end
 end
--- lib/simple.rb
+++ lib/simple.rb
@@ -3,4 +3,14 @@ module SimpleExtensions
     ostart + (ostop - ostart) * (
         Float((value - istart)) / Float((istop - istart)))
   end
+
+  def normalized_average average, new_value, old_units, new_units
+    if new_units == old_units
+      new_average = average - Float(average) / old_units
+      new_average += Float(new_value) / old_units
+    else
+      new_average = Float(average) / new_units * old_units
+      new_average += Float(new_value) / new_units * (new_units - old_units)
+    end
+  end
 end
