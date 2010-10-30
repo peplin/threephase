8c370618aa9cdaceeb3442c7d4b46aa9d74bcbfa
Christopher Peplin <peplin@bueda.com>
2010-10-30 17:49:54 -0400

    Add closest_to finder for market prices.

--- app/models/market_price.rb
+++ app/models/market_price.rb
@@ -2,6 +2,11 @@ class MarketPrice < ActiveRecord::Base
   belongs_to :game
   belongs_to :fuel_market
 
+  scope :closest_to, lambda { |date|
+    order(%{ABS(strftime('%s', "#{date.to_formatted_s(:db)}") - strftime('%s', created_at)) asc}).
+    limit(1)
+  }
+
   validates :price, :presence => true
   validates :game, :presence => true
   validates :fuel_market, :presence => true
--- spec/models/market_price_spec.rb
+++ spec/models/market_price_spec.rb
@@ -6,4 +6,13 @@ describe MarketPrice do
   it { should validate_presence_of :game }
   it { should validate_presence_of :fuel_market }
   it { should validate_presence_of :price }
+
+  context "A MarketPrice instance" do
+    it "should find the price closest to a time" do
+      price = Factory :market_price, :created_at => 6.hours.ago
+      another_price = Factory :market_price, :created_at => 12.hours.ago
+      MarketPrice.closest_to(7.hours.ago).first.should eq(price)
+      MarketPrice.closest_to(12.hours.ago).first.should eq(another_price)
+    end
+  end
 end
