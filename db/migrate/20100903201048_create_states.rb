556bc7992c63fe008e534dae45fb07f773d26746
Christopher Peplin <peplin@bueda.com>
2010-10-31 19:30:17 -0400

    Default charged_at times to creation time.

--- db/migrate/20100903201048_create_states.rb
+++ db/migrate/20100903201048_create_states.rb
@@ -4,8 +4,9 @@ class CreateStates < ActiveRecord::Migration
       t.string :name, :null => false
       t.integer :research_budget, :null => false, :default => 5000000
       t.integer :cash
-      t.datetime :stepped_at
-      t.datetime :customers_charged_at
+      t.datetime :stepped_at, :default => Time.now
+      t.datetime :customers_charged_at, :default => Time.now
+      t.datetime :costs_deducted_at, :default => Time.now
 
       t.references :map, :null => false
       t.references :game, :null => false
