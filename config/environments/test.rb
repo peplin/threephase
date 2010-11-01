d8037b1ae7c11f170eabcb7fedb7b904156cfdd7
Christopher Peplin <peplin@bueda.com>
2010-11-01 11:42:12 -0400

    Set redis URL in test environment as well.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,6 @@ Threephase::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
 end
