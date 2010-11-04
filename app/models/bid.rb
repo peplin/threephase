58b300b05efb562dd781f6dcf6f9c4a37ac5a531
Christopher Peplin <peplin@bueda.com>
2010-11-04 00:42:59 -0400

    Remove duplicated validator.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,13 +1,3 @@
-class LineEndpointValidator < ActiveModel::Validator
-  def validate record
-    starting = record.send(options[:start])
-    ending = record.send(options[:end])
-    if starting and ending and starting == ending
-      record.errors[:base] << "start and endpoint cannot be the same"
-    end
-  end
-end
-
 class Bid < ActiveRecord::Base
   belongs_to :generator
 
